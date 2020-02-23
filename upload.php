9df8c94d5e053d070e8a970d4f84377acc1eea75
Daichi Kitamura <d-kitamura@ieee.org>
2020-02-23 14:37:20 +0900

    ファイルの最終更新日時も画面に表示する機能を追加

--- upload.php
+++ upload.php
@@ -3,6 +3,7 @@ header('Content-Type: text/html; charset=UTF-8');
 $tempfile = $_FILES['fname']['tmp_name'];
 $dir = './uploadedFile/'; // アップロード先のディレクトリ名
 $filename = $dir . $_FILES['fname']['name']; // ディレクトリ名＋ファイル名
+date_default_timezone_set('Asia/Tokyo'); // タイムゾーンを設定（ファイルの更新日時表示のため）
 
 if ( is_uploaded_file($tempfile) ) { //ファイルがアップロードされている（is_uploaded_fileがtrue）
     if( preg_match('/\d{2}_st\d{2}_ver[1-9]\.(?i)pdf/', $_FILES['fname']['name']) ){ //正規表現とマッチする（preg_matchがtrue，\d{2}は二桁の半角数字，[1-9]は1～9の半角数字1桁，(?i)は以後大文字小文字を区別しない）
@@ -24,12 +25,13 @@ else { //ファイルがアップロードされていない（is_uploaded_file�
 }
 
 // ディレクトリの内容を読み込み（学生は自分のファイルがアップされたかどうか不安になるので，現在アップされいるファイル一覧を確認できるよう表示）
-echo "現在サーバにアップロードされているファイル一覧：<br>";
+echo "現在サーバにアップロードされているファイル一覧と最終更新日時：<br>";
 if (is_dir($dir)) {
     if ($dh = opendir($dir)) {
         while (($file = readdir($dh)) !== false) {
             if ( strcmp($file, ".") !== 0 && strcmp($file, "..") !== 0){ // .と..は表示しない
-                echo "　- " . $file . "<br>";
+                $filetime = date("Y-m-d H:i:s", filemtime( $dir . $file )); // ファイルの更新時刻取得しdate関数で時刻を整えて出力（$dir.$fileがファイルパス）
+                echo "　- " . $file . " : " . $filetime . "<br>";
             }
         }
         closedir($dh);
